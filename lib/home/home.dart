94329db9d37cfee0a265eee03dc9a092dcd01584
ImNotAOwl <siegel.matthieu@ymail.com>
2022-04-11 23:27:06 +0200

    add form in home for login

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -10,6 +10,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,9 +20,44 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'Coucou ! You have pushed the button this many times:',
+          children: <Widget>[
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: 'Email',
+                    ),
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty) {
+                        return "Il manque votre email";
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      hintText: 'Password',
+                    ),
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty) {
+                        return "Il manque votre mot de passe";
+                      }
+                      return null;
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: ElevatedButton(
+                      onPressed: () =>
+                          {if (_formKey.currentState!.validate()) {}},
+                      child: const Text("Login"),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),

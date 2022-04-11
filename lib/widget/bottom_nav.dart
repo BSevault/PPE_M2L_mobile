2ce5409e02e3e875225ca0572127898688a8e511
ImNotAOwl <siegel.matthieu@ymail.com>
2022-04-11 23:00:42 +0200

    add a bottom nav bar

---
+++ lib/widget/bottom_nav.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({Key? key}) : super(key: key);
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.check),
+          label: 'Participants',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.food_bank_sharp),
+          label: 'Services',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.medical_services_sharp),
+          label: 'Covid',
+        ),
+      ],
+      currentIndex: _selectedIndex,
+      selectedItemColor: Colors.lightGreen,
+      onTap: _onItemTapped,
+    );
+  }
+}

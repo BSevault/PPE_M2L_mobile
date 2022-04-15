aecbe6254b73e41c04fa935fb728e3006550d2af
Guillaume Saulnier <hirona_mj@hotmail.fr>
2022-04-15 08:59:48 +0200

    update lambda

--- lib/home/test.dart
+++ lib/home/test.dart
@@ -23,25 +23,25 @@ class _TestState extends State<Test> {
           padding: const EdgeInsets.all(16.0),
           child: FutureBuilder(
             // future: Requester.getRequest('/flutter/1/reservations'),
-            future: Requester.getRequest('/flutter/1/reservations/history'),
-            // future: Requester.postRequest('/flutter/login',
-            //     {"email": "test1@email.com", "password": "test1"}),
+            // future: Requester.getRequest('/flutter/1/reservations/history'),
+            future: Requester.postRequest('/flutter/login',
+                {"email": "test1@email.com", "password": "test1"}),
             builder: (BuildContext ctx, AsyncSnapshot snapshot) {
               if (snapshot.data == null) {
                 return const Center(
                   child: CircularProgressIndicator(),
                 );
               } else {
+                var users = [snapshot.data];
                 // var users = snapshot.data[0];
-                var users = snapshot.data[0];
                 print(users);
                 return ListView.builder(
                   itemCount: users.length,
                   itemBuilder: (ctx, index) => ListTile(
                     title: Text(users[index]['nom']),
-                    subtitle: Text(users[index]['is_covid'] == 1
-                        ? 'Is covid'
-                        : 'Not covid'),
+                    // subtitle: Text(users[index]['is_covid'] == 1
+                    //     ? 'Is covid'
+                    //     : 'Not covid'),
                     contentPadding: const EdgeInsets.only(bottom: 20.0),
                   ),
                 );
